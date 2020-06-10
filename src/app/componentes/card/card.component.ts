9f2896e9c2a314a91eadf788a9d423d27f9b2da4
Jose Vargas <ruedagato@gmail.com>
2020-06-10 16:27:13 -0500

    create json data

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  image = 'https://www.biography.com/.image/t_share/MTU0OTkwNDUxOTQ5MDUzNDQ3/kanye-west-attends-the-christian-dior-show-as-part-of-the-paris-fashion-week-womenswear-fall-winter-2015-2016-on-march-6-2015-in-paris-france-photo-by-dominique-charriau-wireimage-square.jpg'
+  image = ''
   like = 50;
   time = 3;
   job = 'Entertainment'
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonModel } from 'src/core/person.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  persons: PersonModel[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    fetch('/assets/data.json')
+      .then((resp) => resp.json())
+      .then((data) => (this.persons = data));
   }
 
+  ngOnInit(): void {}
 }
